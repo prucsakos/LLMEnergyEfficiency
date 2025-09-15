728fd8ec92931faec25fed885308e4f57310a8f9
Akos Prucs <prucsakos@gmail.com>
2025-09-15 07:17:56 +0000

    env setup instructions

--- scripts/install_notes.sh
+++ scripts/install_notes.sh
@@ -1,3 +1,7 @@
+
 uv pip install -U vllm \
     --torch-backend=auto \
     --extra-index-url https://wheels.vllm.ai/nightly
+uv pip install falshinfer-python
+uv pip uninstall pynvml
+uv pip install nvidia-ml-py
