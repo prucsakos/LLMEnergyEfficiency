c5286d3927512ebed8d9fa7434faced89051a54d
Prucs Akos <prucsakos@gmail.com>
2025-10-15 16:56:27 +0200

    Update fix_missing_nvcc.sh to append CUDA env vars to ~/.bashrc

--- scripts/fix_missing_nvcc.sh
+++ scripts/fix_missing_nvcc.sh
@@ -2,3 +2,9 @@
 
 export PATH=/usr/local/cuda/bin:$PATH
 export CUDA_HOME=/usr/local/cuda
+
+# Append to ~/.bashrc for persistence
+echo "export PATH=/usr/local/cuda/bin:\$PATH" >> ~/.bashrc
+echo "export CUDA_HOME=/usr/local/cuda" >> ~/.bashrc
+
+echo "CUDA environment variables have been set and added to ~/.bashrc"
